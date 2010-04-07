f438588688d191af9551b5d60898070bbc8add49
John Hobbs <john@velvetcache.org>
2010-04-07 11:10:21 -0500

    Updated config. I'm going to bail on this whole 'framework' thing.

--- protected/config.php
+++ protected/config.php
@@ -1,4 +1,10 @@
 <?php
 	$config = array();
 	$config['uri_prefix'] = '/mklst/';
+	
 	$config['default_controller'] = 'page';
+	
+	$config['database_host'] = 'localhost';
+	$config['database_user'] = 'mklst';
+	$config['database_password'] = 'mklst';
+	$config['database_name'] = 'mklst';
