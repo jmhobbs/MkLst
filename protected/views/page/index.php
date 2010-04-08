f39b52987db105a435cb39344b7edf643630ebcb
John Hobbs <jmhobbs@pristina.dreamhost.com>
2010-04-08 15:11:23 -0700

    Fixed cache clash, added some more detail on the deployed home page.

--- protected/views/page/index.php
+++ protected/views/page/index.php
@@ -8,10 +8,16 @@
 	Ugly but functional.
 </p>
 
+<h2>Try It</h2>
 <p>
 	<form action="<?php echo uri::path( 'list/create' ); ?>">
-		<button>New List +</button>
+		<button>Create A New List</button>
 	</form>
 </p>
 
+<p>
+	Or, see <a href="http://mklst.com/list/view/66e3cba5510364250589121cfc0793dddd31f20b">if I'm keeping up with my lawn work.</a>
+</p>
+
+<h2>Demo Vieo</h2>
 <object width="480" height="385"><param name="movie" value="http://www.youtube.com/v/mW6k4TiJdTg&hl=en_US&fs=1&color1=0x3a3a3a&color2=0x999999"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/mW6k4TiJdTg&hl=en_US&fs=1&color1=0x3a3a3a&color2=0x999999" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="385"></embed></object>
