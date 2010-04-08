286dad0f4864c6a7e6277fc4df5c79f901a7dea7
John Hobbs <john@velvetcache.org>
2010-04-08 17:02:33 -0500

    Move config and stuff to templates to ease deployment merges.

--- protected/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$config = array();
-	$config['uri_prefix'] = '/mklst/';
-	$config['default_controller'] = 'page';
--- protected/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$config = array();
-	$config['uri_prefix'] = '/mklst/';
-	$config['default_controller'] = 'page';
-	$config['list_salt'] = 'mklst';
