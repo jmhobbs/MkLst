7818e94616bef0e9ed05c2b8115c3003096c7d0a
John Hobbs <john@velvetcache.org>
2010-04-08 16:57:31 -0500

    Sync content with live site.

--- protected/views/page/index.php
+++ protected/views/page/index.php
@@ -13,3 +13,5 @@
 		<button>New List +</button>
 	</form>
 </p>
+
+<object width="480" height="385"><param name="movie" value="http://www.youtube.com/v/mW6k4TiJdTg&hl=en_US&fs=1&color1=0x3a3a3a&color2=0x999999"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/mW6k4TiJdTg&hl=en_US&fs=1&color1=0x3a3a3a&color2=0x999999" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="385"></embed></object>
