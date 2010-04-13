42632baed5ffacf9953a30f835540b051c8e5ec6
John Hobbs <john@velvetcache.org>
2010-04-13 17:32:17 -0500

    Updated about page and index.

--- protected/views/page/about.php
+++ protected/views/page/about.php
@@ -1,20 +1,18 @@
 <h1>MkLst</h1>
 
 <p>MkLst.com is dead simple lists you can print and save.  You can edit them anonymously, you can protect them, you can share them.</p>
-<p>The idea is to trim the fat down to something you will use 100% of the features on.</p>
-
-<h1>Why?</h1>
-
-<p>To scratch my own itch. I wanted lists I could print (I know, horrible, right?) and reorder quickly.</p>
-
-<p>The reason this project is so "complicated" is that I also wanted to try writing my own MVC framework, which I did and stuffed into this project.</p>
+<p>The idea is to trim the fat down to something you will actually use 100% of the features on.</p>
+<p>No sub-lists, no labels, no folders, no task assignment, just lists.</p>
 
 <h1>Credits</h1>
 
 <p>ORM provided by CoughPHP - FreeBSD License</p>
 <p>JavaScript awesomeness by jQuery - MIT License</p>
+<p>Everything else is written by me, John Hobbs of <a href="http://www.littlefilament.com/" target="_blank">Little Filament</a>.</p>
 
-<h1>License</h1>
+<h1>Yes, it's open source.</h1>
+
+<p>MkLst is released under the MIT License. Enjoy.</p>
 
 <pre>
 Copyright (c) 2010 John Hobbs
--- protected/views/page/index.php
+++ protected/views/page/index.php
@@ -5,19 +5,16 @@
 </p>
 
 <p>
-	Ugly but functional.
+	<a href="<?php echo uri::path( 'page/about' ); ?>">Learn More</a>
 </p>
 
 <h2>Try It</h2>
 <p>
 	<form action="<?php echo uri::path( 'list/create' ); ?>">
 		<button>Create A New List</button>
+		or, see <a href="http://mklst.com/list/view/66e3cba5510364250589121cfc0793dddd31f20b">if I'm keeping up with my lawn work.</a>
 	</form>
 </p>
 
-<p>
-	Or, see <a href="http://mklst.com/list/view/66e3cba5510364250589121cfc0793dddd31f20b">if I'm keeping up with my lawn work.</a>
-</p>
-
-<h2>Demo Video</h2>
+<h2>See It</h2>
 <object width="480" height="385"><param name="movie" value="http://www.youtube.com/v/mW6k4TiJdTg&hl=en_US&fs=1&color1=0x3a3a3a&color2=0x999999"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/mW6k4TiJdTg&hl=en_US&fs=1&color1=0x3a3a3a&color2=0x999999" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="385"></embed></object>
